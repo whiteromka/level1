1ec341517a3b9be4862a58b477a626bc6f750ea4
whiteromka <whiteromka@yandex.ru>
2024-06-04 11:37:28 +0300

    lesson12

--- catForm/form.php
+++ catForm/form.php
@@ -3,8 +3,8 @@
 require_once '../funcs.php';
 
 // Это для отладки
-debug($_GET);
-echo "<hr>";
+//debug($_GET);
+//echo "<hr>";
 
 // Основная логика начинается отсюда
 $successMessage = '';
@@ -118,122 +118,136 @@ function getSavedCats(): array
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Котаформа</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
 </head>
 <body>
-<h3>Котаформа</h3>
-<div>
-    <form method="GET" action="">
-        <p>
-            Cat name here:
-            <input type="text" name="name" placeholder="Cat name!!!" value="<?php echo getDataFromGET('name'); ?>">
-        </p>
 
-        <p>
-            Cat age here:
-            <input type="text" name="age" placeholder="Cat age" value="<?php echo getDataFromGET('age'); ?>">
-        </p>
+<div class="container myCont">
 
-        <p>
-            Cat gender:
-            <label>
-                <select name="gender">
+    <div class="row myRow">
+        <div class="col myCol">1 из 2</div>
+        <div class="col myCol">2 из 2</div>
+    </div>
+
+    <div class="row myRow">
+        <div class="col-1 myCol">1 из 3</div>
+        <div class="col-2 myCol">2 из 3</div>
+        <div class="col-9 myCol">3 из 3</div>
+    </div>
+
+    <div class="row myRow">
+        <div class="col myCol">1 из 1</div>
+    </div>
+
+    <h3 class="header">Котаформа</h3>
+    <div>
+        <form method="GET" action="">
+            <div class="form-group">
+                <label for="catNameInput">Cat name here</label>
+                <input type="text" class="form-control" name="name" id="catNameInput"
+                       value="<?php echo getDataFromGET('name'); ?>">
+            </div>
+            <div class="form-group">
+                <label for="catAgeInput">Cat age here</label>
+                <input type="text" name="age" class="form-control" id="catAgeInput"
+                       value="<?php echo getDataFromGET('age'); ?>">
+            </div>
+            <p>
+                Cat gender:
+                <label>
+                    <select name="gender">
+                        <?php
+                        foreach ($genderList as $genderValue) {
+                            $selected = '';
+                            if ($genderValue == getDataFromGET('gender')) {
+                                $selected = 'selected';
+                            }
+
+                            echo "<option $selected value='$genderValue'>$genderValue</option>";
+                        }
+                        ?>
+                    </select>
+                </label>
+            </p>
+            <p> Cat sociability:
+                <select name="sociability">
                     <?php
-                    foreach ($genderList as $genderValue) {
+                    foreach ($sociabilityList as $sociabilityValue) {
                         $selected = '';
-                        if ($genderValue == getDataFromGET('gender')) {
+                        if ($sociabilityValue == getDataFromGET('sociability')) {
                             $selected = 'selected';
                         }
-                        /*
-                        if (empty(getDataFromGET('gender')) && $genderValue == 'Boy') {
-                            $selected = 'selected';
-                        }
-                        */
-                        echo "<option $selected value='$genderValue'>$genderValue</option>";
+
+                        echo "<option $selected value='$sociabilityValue'>$sociabilityValue</option>";
                     }
                     ?>
                 </select>
-            </label>
-        </p>
+            </p>
 
-        <p> Cat sociability:
-            <select name="sociability">
-                <?php
-                foreach ($sociabilityList as $sociabilityValue) {
-                    $selected = '';
-                    if ($sociabilityValue == getDataFromGET('sociability')) {
-                        $selected = 'selected';
-                    }
-                    /*
-                    if (empty(getDataFromGET('sociability')) && $sociabilityValue == 5) {
-                        $selected = 'selected';
-                    }
-                    */
-                    echo "<option $selected value='$sociabilityValue'>$sociabilityValue</option>";
-                }
-                ?>
-            </select>
-        </p>
+            <p>
+                <input type="submit" class="btn btn-outline-primary" value="Save this cat in DB">
+                <a class="btn btn-outline-secondary" href="/catform/form.php">Clear</a>
+            </p>
+            <?php
+            // empty === true   // 0, false, null, '', '0', []
+            if (!empty($successMessage)) {
+                echo '<div class="alert" style="background: #a1f85b" role="alert">
+                   ' . $successMessage . '
+                </div>';
+            }
+            if (!empty($errorMessage)) {
+                echo "<p style='color:red'> $errorMessage </p>";
+            }
+            ?>
+        </form>
+    </div>
+    <hr>
 
-        <p>
-            <input type="submit" value="Save this cat in DB">
-
-            <a
-                style="color: black; background: #ff0000; border: 1px solid #002bff; text-decoration: none; padding: 0px 10px"
-                href="/catform/form.php"
-            >
-                Clear
-            </a>
-        </p>
+    <div>
+        <h3>Saved cats:</h3>
         <?php
-        // empty === true   // 0, false, null, '', '0', []
-        if (!empty($successMessage)) {
-            echo "<p style='color: green'>$successMessage</p>";
-        }
-        if (!empty($errorMessage)) {
-            echo "<p style='color:red'> $errorMessage </p>";
+        $cats = getSavedCats();
+        foreach ($cats as $cat) {
+            // строка ... {$cat['name']} ... остальная строка ... // { ... } позволяют внедрить в строку сложные переменные
+            $n = $cat['name'];
+            echo "<p> 
+                        Name: <b>$n</b>, 
+                        Age: <b>" . $cat['age'] . "</b>, 
+                        Gender: <b>{$cat['gender']}</b>, 
+                        Sociability: <b>{$cat['sociability']}</b> 
+                </p>";
         }
         ?>
-    </form>
-</div>
-<hr>
-
-<div>
-    <h3>Saved cats:</h3>
-    <?php
-    $cats = getSavedCats();
-    foreach ($cats as $cat) {
-        // строка ... {$cat['name']} ... остальная строка ... // { ... } позволяют внедрить в строку сложные переменные
-        $n = $cat['name'];
-        echo "<p> 
-                    Name: <b>$n</b>, 
-                    Age: <b>" . $cat['age'] . "</b>, 
-                    Gender: <b>{$cat['gender']}</b>, 
-                    Sociability: <b>{$cat['sociability']}</b> 
-            </p>";
-    }
-    ?>
+    </div>
 </div>
 </body>
 </html>
 
 <!--
 
-ДЗ урока 11.
+ДЗ урока 12.
 
-1) Любую задачу с кодварс. А лучше 3 задачи с кодварс
+1) Любую задачу с кодварс
 
-2) По аналогии с этой котаформой нужно создать свою, желательно какаую-нибудь
-полезную или интересную вам.
-Например форму для подсчета съеденных продуктов и калорий за день,
-или форма для сохранения наиболее интересных вакансий с hh.ru
-или форма для сохранения друзей и их телефонов,
-или форма для сохранения данных о машинах,
-или данных о странах.
-Все что угодно, все что вам может пригодится или интересует вас.
-Все инпуты, их тип, количество и логика работы формы все по вашему усмотрению.
-Нужно обязательно сделать что бы данные из формы сохранялись в файл.
-И сохраненные данные должны выводится на страницу!
+2) В котаформе нужно домучать 2-ва инпута, "Cat gender" и "Cat sociability". Преобразовать их в бутстраповкие инпуты-дропдауны
+
+3 а) Блоке где выводятся сохраненные коты "Saved cats". Нужно переписать этот блок так что бы все сохраненные коты выводились
+как в этом примере с бутстрап уведомленими https://getbootstrap.ru/docs/4.4/components/alerts/
+Т.е.  каждого кота выводить в отдельном блоке-уведомлении со своим цветом.
+Коты у которых социальность выше 7 должны выводиться в зеленом блоке.
+Коты у которых социальность ниже 2 должны выводиться в сером блоке.
+Коты у которых социальность от 2 до 3 должны выводиться в красном блоке.
+Чтонибуть похожее с синим и желтым блоком, на ваш вкус.
+
+3 б) Если уверены в своих силах можно попробовать вывести котов через карточки https://getbootstrap.ru/docs/4.4/components/card/ и систему сеток https://getbootstrap.ru/docs/4.4/layout/grid/
+
+
+4) https://getbootstrap.ru/docs/4.4/components/alerts/  просмотреть примеры / прокликать ссылки в левом вертикальном меню компонентов бутстрапа.
+
 
 -->
-
-
